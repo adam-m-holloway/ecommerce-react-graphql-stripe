14eb1d825e1f7d13be3db81d48b2b722731ca846
Adam Holloway <adam.holloway21@gmail.com>
2020-01-10 12:37:42 +0000

    chore: add strapi api call to client

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ http://localhost:1337/admin/
 
 `/server` folder contains all the CMS.
 
-Use `npm run development` from the `server` folder to run the CMS locally.
+Use `npm run develop` from the `server` folder to run the CMS locally.
 
 Admin area - http://localhost:1337/admin
 GraphQL Playground - http://localhost:1337/graphql
--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,3 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Container, Box, Heading } from 'gestalt';
+import './App.css';
+import Strapi from 'strapi-sdk-javascript/build/main';
 
-export const App = () => <div>App</div>;
+const apiUrl = process.env.API_URL || 'http://localhost:1337/';
+const strapi = new Strapi(apiUrl);
+
+export const App = () => {
+  const [brands, setBrands] = useState({ brands: [] });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await strapi.request('POST', '/graphql', {
+          data: {
+            query: `query {
+                brands {
+                  _id
+                  name
+                  description
+                  image {
+                    url
+                  }
+                }
+              }`
+          }
+        });
+
+        setBrands({ brands: response.data.brands });
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <Container>
+      <Box display="flex" justifyContent="center" marginBottom={2}>
+        <Heading color="midnight" size="md">
+          Brew Brands
+        </Heading>
+      </Box>
+    </Container>
+  );
+};
