3bdd33b402998f630ea52f4da401a0b5c0839f10
Adam Holloway <adam.holloway21@gmail.com>
2020-01-13 15:39:06 +0000

    chore: add content to page; add search filter

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,15 @@
 import React, { useEffect, useState } from 'react';
-import { Container, Box, Heading } from 'gestalt';
+import {
+  Container,
+  Box,
+  Heading,
+  Card,
+  Image,
+  Text,
+  SearchField,
+  Spinner
+} from 'gestalt';
+import { Link } from 'react-router-dom';
 import './App.css';
 import Strapi from 'strapi-sdk-javascript/build/main';
 
@@ -7,7 +17,9 @@ const apiUrl = process.env.API_URL || 'http://localhost:1337/';
 const strapi = new Strapi(apiUrl);
 
 export const App = () => {
-  const [brands, setBrands] = useState({ brands: [] });
+  const [brands, setBrands] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [loadingBrands, setLoadingBrands] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -27,22 +39,83 @@ export const App = () => {
           }
         });
 
-        setBrands({ brands: response.data.brands });
+        setBrands(response.data.brands);
+        //setLoadingBrands(false);
       } catch (err) {
         console.error(err);
+        setLoadingBrands(false);
       }
     };
 
     fetchData();
   }, []);
 
+  const handleChange = ({ value }) => {
+    setSearchTerm(value);
+  };
+
+  const filteredBrands = !searchTerm
+    ? brands
+    : brands.filter(
+        brand =>
+          brand.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+          brand.description.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+
   return (
     <Container>
+      <Box display="flex" justifyContent="center" margin-top={4}>
+        <SearchField
+          id="searchField"
+          onChange={handleChange}
+          placeholder="Search Brands"
+          accessibilityLabel="search"
+          value={searchTerm}
+        />
+      </Box>
       <Box display="flex" justifyContent="center" marginBottom={2}>
         <Heading color="midnight" size="md">
           Brew Brands
         </Heading>
       </Box>
+      {!loadingBrands ? (
+        <Spinner show={loadingBrands} accessibilityLabel="Loading Spinner" />
+      ) : (
+        <Box display="flex" justifyContent="around">
+          {filteredBrands.map(brand => (
+            <Box width={200} margin={2} key={brand._id}>
+              <Card
+                image={
+                  <Box height={200} width={200}>
+                    <Image
+                      alt="Brand"
+                      naturalHeight={1}
+                      naturalWidth={1}
+                      src={`${apiUrl}${brand.image[0].url}`}
+                    />
+                  </Box>
+                }
+              >
+                <Box
+                  display="flex"
+                  alignItems="center"
+                  justifyContent="center"
+                  direction="column"
+                >
+                  <Text bold size="xl">
+                    {brand.name}
+                  </Text>
+                  <Text>{brand.description}</Text>
+                  <Text bold size="xl">
+                    <Link to={`/${brands._id}`}>See Brews</Link>
+                  </Text>
+                </Box>
+              </Card>
+            </Box>
+          ))}
+        </Box>
+      )}
+      ;
     </Container>
   );
 };
